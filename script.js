1fa473ce26a5a14c74af70f4c38c5c05989cf74b
pryntl <pryntl2007@gmail.com>
2025-02-09 14:40:16 +0330

    feat js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+let jj = [2, 5, 7, 10];
 
+console.log(
+  jj.map((number) => {
+    return number * 2;
+  })
+);
+console.log(
+  jj.filter((number) => {
+    return number >= 7;
+  })
+);
+console.log(
+  jj.reduce((firstNum, lastNum) => {
+    return firstNum - lastNum;
+  })
+);
